8b1ddb6833717b2428ea0b8335164f34ad671aa2
Miqueias Caetano <miqueias.caetano@vistasoft.com.br>
2022-06-18 09:22:03 -0300

    ok

--- platforms/android/app/build/generated/source/buildConfig/debug/com/phonegap/climahojemobile/BuildConfig.java
+++ platforms/android/app/build/generated/source/buildConfig/debug/com/phonegap/climahojemobile/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.phonegap.climahojemobile";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 10027;
-  public static final String VERSION_NAME = "1.0.27";
+  public static final int VERSION_CODE = 10032;
+  public static final String VERSION_NAME = "1.0.32";
 }
--- platforms/android/app/build/generated/source/buildConfig/release/com/phonegap/climahojemobile/BuildConfig.java
+++ platforms/android/app/build/generated/source/buildConfig/release/com/phonegap/climahojemobile/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "com.phonegap.climahojemobile";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 10029;
-  public static final String VERSION_NAME = "1.0.29";
+  public static final int VERSION_CODE = 10032;
+  public static final String VERSION_NAME = "1.0.32";
 }
